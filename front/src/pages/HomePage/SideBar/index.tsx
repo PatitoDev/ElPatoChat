18d17acd8f904840cbf0bbbe38aa674ca291eddc
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-07-23 17:44:00 +0100

    upgrade tailwind and zustand

--- front/src/pages/HomePage/SideBar/index.tsx
+++ front/src/pages/HomePage/SideBar/index.tsx
@@ -21,28 +21,28 @@ export const SideBar = ({ onChange, selectedSection }: SideBarProps) => {
     >
       <button 
         onClick={() => onChange(ConfigurationSection.ChatVisual)} 
-        className={` ${selectedSection === ConfigurationSection.ChatVisual ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded p-2`}
+        className={` ${selectedSection === ConfigurationSection.ChatVisual ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded-sm p-2`}
       >
         <img width={20} height={20} src='img/MingcuteSettings4Line.svg'></img>
       </button>
 
       <button
         onClick={() => onChange(ConfigurationSection.IgnoredUsers)} 
-        className={` ${selectedSection === ConfigurationSection.IgnoredUsers ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded p-2`}
+        className={` ${selectedSection === ConfigurationSection.IgnoredUsers ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded-sm p-2`}
       >
         <span className="icon-[mingcute--user-forbid-line] w-[20px] h-[20px]" role="img" aria-hidden="true" />
       </button>
 
       <button
         onClick={() => onChange(ConfigurationSection.NameReplacement)} 
-        className={` ${selectedSection === ConfigurationSection.NameReplacement ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded p-2`}
+        className={` ${selectedSection === ConfigurationSection.NameReplacement ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded-sm p-2`}
       >
         <img width={20} height={20} src='img/MingcuteUserEditLine.svg'></img>
       </button>
 
       <button
         onClick={() => onChange(ConfigurationSection.TTS)} 
-        className={` ${selectedSection === ConfigurationSection.TTS ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded p-2`}
+        className={` ${selectedSection === ConfigurationSection.TTS ? 'bg-[#2e2e2e]' : ''} hover:bg-[#4d4d4d] rounded-sm p-2`}
       >
         <img width={20} height={20} src='img/MingcuteChat2Line.svg'></img>
       </button>
