97e6198bf83b24ee6410cfb1d77fe4b1cc88af74
PatitoDev <115660028+PatitoDev@users.noreply.github.com>
2025-07-23 18:56:34 +0100

    update default config

--- front/src/store/defaultConfiguration.ts
+++ front/src/store/defaultConfiguration.ts
@@ -9,20 +9,14 @@ const ttsDefaultConfig: TTSConfiguration = {
     { id: crypto.randomUUID(), ordinal: 0, regex: 'pierito95rsng', replaceWith: 'pierito', regexFlags: '', description: '' },
     { id: crypto.randomUUID(), ordinal: 1, regex: 'lam277', replaceWith: 'lam', regexFlags: '', description: '' },
     { id: crypto.randomUUID(), ordinal: 2, regex: 'inc0gn1t_94610', replaceWith: 'incognito', regexFlags: '', description: '' },
-    { id: crypto.randomUUID(), ordinal: 3, regex: 'ckmu32', replaceWith: 'cristi', regexFlags: '', description: '' },
     { id: crypto.randomUUID(), ordinal: 4, regex: 'bulbsum', replaceWith: 'bulbi', regexFlags: '', description: '' },
     { id: crypto.randomUUID(), ordinal: 5, regex: 'guerra24_', replaceWith: 'lili', regexFlags: '', description: '' },
     { id: crypto.randomUUID(), ordinal: 6, regex: 'ssmatiuri', replaceWith: 'matilda', regexFlags: '', description: '' }
   ],
   replacements: [
     { id: crypto.randomUUID(), ordinal: 0, regex: '^!.*', replaceFullMessage: true, replaceWith: '', regexFlags: '', description: 'Don\'t read messages starting with !'  },
-    { id: crypto.randomUUID(), ordinal: 1, regex: '\\*quack\\*', regexFlags: 'gi', replaceWith: '', description: 'Don\'t read *quack*' },
-    { id: crypto.randomUUID(), ordinal: 2, regex: '[g|j]+h*a+h*n+h*( ?)+[szx]+( ?)+o+', regexFlags: 'gi', replaceWith: 'pato', description: 'Replace ganso with pato' },
-    { id: crypto.randomUUID(), ordinal: 3, regex: 'cks+[o|ó|O|Ó]', regexFlags: 'gi', replaceWith: 'se caso', description: '' },
     { id: crypto.randomUUID(), ordinal: 4, regex: '[\\uD83C-\\uFFFF]', regexFlags: 'g', replaceWith: '',  description: 'Don\'t read emojis (does not cover all emojis)' },
     { id: crypto.randomUUID(), ordinal: 5, regex: '_*', regexFlags: 'g', replaceWith: '', description: 'Don\'t read underscores' },
-    { id: crypto.randomUUID(), ordinal: 6, regex: '\\bck\\b', regexFlags: 'gi', replaceWith: 'cristi', description: 'Read ck as cristi' },
-    { id: crypto.randomUUID(), ordinal: 7, regex: '\\bckmu\\b', regexFlags: 'gi',  replaceWith: 'cristi', description: 'Read ckmu as cristi' },
     { id: crypto.randomUUID(), ordinal: 8, regex: '[0-9a-zA-z]\\.[a-zA-Z][a-zA-Z]', replaceFullMessage: true, replaceWith: '$who a enviado un link.',  regexFlags: '', description: 'Replace links' },
     { id: crypto.randomUUID(), ordinal: 9, regex: '^\\*.+\\*$', replaceWith: '$who $msg', replaceFullMessage: true, description: '', regexFlags: '',
       replacement: {
@@ -43,8 +37,6 @@ export const defaultUserConfiguration: UserConfiguration = {
   ignoredUsers: [
     { id: crypto.randomUUID(), value: 'nightbot' },
     { id: crypto.randomUUID(), value: 'streamelements' },
-    { id: crypto.randomUUID(), value: 'el_pato_bot' },
-    { id: crypto.randomUUID(), value: 'ckmu_bot' },
   ],
   betterTTVEnabled: true,
   chatDirection: 'right',
